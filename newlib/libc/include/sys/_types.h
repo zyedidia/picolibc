3664a360700b8dd7e9f923d0d431a6a31d640597
Zachary Yedidia <zyedidia@gmail.com>
2024-02-06 18:04:12 -0800

    Define wint_t

--- newlib/libc/include/sys/_types.h
+++ newlib/libc/include/sys/_types.h
@@ -54,9 +54,7 @@ SUCH DAMAGE.
 /* The Arm Compiler doesn't define wint_t as part of stddef.h so
  * define it here.
  */
-#if defined (__ARMCC_VERSION) && (__ARMCC_VERSION >= 6100100)
 typedef __WINT_TYPE__ wint_t;
-#endif
 
 #include <newlib.h>
 #include <sys/config.h>
